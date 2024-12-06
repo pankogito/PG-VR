ec73a0391dbc6612d57edd441d0e9a3e92fbfa0d
pankogito <103581552+pankogito@users.noreply.github.com>
2024-12-06 12:44:39 +0100

    PGVR-1 Manipulator

---
+++ manipulator/manipulator.gd
@@ -0,0 +1,54 @@
+extends Path3D
+
+@export var hand:Node3D
+
+@export_subgroup("vibration borders")
+@export var vibration_start = 0.01
+@export var vibration_max = 0.05
+@export_subgroup("vibration ratios")
+@export var streach_ratio = 0.1
+@export var jump_ratio = 0.1
+
+
+@onready var handle:Node3D = $Handle
+@onready var handle_visualization:Node3D = $Handle/MeshInstance3D
+
+var vibration 
+func _ready() -> void:
+	_on_curve_changed()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if not hand:
+		return
+	
+	var prevoius_positon = handle.global_position
+	
+	var offset = curve.get_closest_offset(to_local(hand.global_position))
+	handle.progress_ratio = offset / curve.get_baked_length()
+	
+	vibration = streach_ratio*(handle.global_position - hand.global_position).length()
+	
+	vibration += jump_ratio*(handle.global_position - prevoius_positon).length()
+	
+	
+	vibration = vibration-vibration_start
+	
+	vibration = clamp(vibration,0,vibration_max)
+	
+	handle_visualization.position = vibration*Vector3(randf_range(-1,1),0,randf_range(-1,1))
+
+func set_hand(h:Node3D):
+	hand = h
+	if not hand:
+		handle_visualization.position = Vector3()
+
+func _on_curve_changed() -> void:
+	var count:float = 0
+	var markers = get_child_count()-1
+	for marker in get_children():
+		if marker == handle:
+			continue
+		marker.progress_ratio = count/(markers-1)
+		
+		count += 1

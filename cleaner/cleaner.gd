ab09a007d5680936520f624f186fa434cfb5bae2
Krzysztof Kuchta <88138591+KKucht@users.noreply.github.com>
2025-01-16 17:27:17 +0100

    Add package cleaner (#10)

---
+++ cleaner/cleaner.gd
@@ -0,0 +1,12 @@
+extends Area3D
+class_name Cleaner
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Function to handle the body_entered signal
+func _on_body_entered(body: Node3D) -> void:
+	if body is Package:
+		body.queue_free()
+	

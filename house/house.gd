3aa70008c706c374b83a35643c96c8207e4f3562
Krzysztof Kuchta <88138591+KKucht@users.noreply.github.com>
2025-01-26 15:42:14 +0100

    pointer counter to main game (#18)

--- house/house.gd
+++ house/house.gd
@@ -21,5 +21,6 @@ func _on_body_entered(body: Node3D) -> void:
 		add_child(visualization)
 		visualization.global_transform = body.global_transform
 		body.queue_free()
-		package_arrived.emit(self)
+		if not package_arrived_var:
+			package_arrived.emit(self)
 		package_arrived_var = true
